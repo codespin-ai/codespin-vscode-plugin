4b3290b15eb5c1be33ec21ab9c4d839fe01bfb17
Jeswin Kumar <jeswinpk@agilehead.com>
2024-12-08 11:55:27 +0530

    Remove unused file

--- src/ui/chat/html/pages/provider/types.ts
+++
@@ -1,8 +0,0 @@
-// import { GenerateUserInput } from "../../../types.js";
-
-// export type SupportedProviders = "openai" | "anthropic" | "google";
-
-// export type ProviderConfigArgs = {
-//   provider: SupportedProviders;
-//   generateUserInput: GenerateUserInput;
-// };
